c7ceb34c24ed97d137b680f42647fe0bbca1ba05
criticalumami <criticalumami@protonmail.com>
2024-04-29 16:05:44 +0300

    new

--- WEBM/js/qgis2web_expressions.js
+++ WEBM/js/qgis2web_expressions.js
@@ -754,3 +754,17 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
+
+
+
+function exp_label_L5_19_eval_expression(context) {
+    // name_en
+
+    var feature = context.feature;
+    
+    if (feature.properties) {
+        return feature.properties['name_en'] ;
+    } else {
+        return feature['name_en'] ;
+    }
+}
--- WEBM/qgis2web_2024_04_28-11_31_08_717642/data/RIVER_1.js
+++
@@ -1,8 +0,0 @@
-var json_RIVER_1 = {
-"type": "FeatureCollection",
-"name": "RIVER_1",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-
-]
-}
